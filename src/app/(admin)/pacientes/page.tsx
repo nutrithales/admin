0b7c0b9cdc1f6f3edda918d506a1331887a6d59f
nutrithales <nutri.thalesrosa@gmail.com>
2026-08-05 12:34:27 -0300

    Delete src directory

--- src/app/(admin)/pacientes/page.tsx
+++
@@ -1,9 +0,0 @@
-import { listPacientes } from "@/services/pacientes.queries";
-import { PacientesClient } from "./PacientesClient";
-
-export const metadata = { title: "Pacientes" };
-
-export default async function PacientesPage() {
-  const pacientes = await listPacientes();
-  return <PacientesClient initialPacientes={pacientes} />;
-}
--- src/utils/constants.ts
+++
@@ -1,9 +0,0 @@
-export const TIPOS_PAGINA_SUGERIDOS = [
-  "dashboard",
-  "diario_alimentar",
-  "alongamentos",
-  "treino_musculacao",
-  "treino_hiit",
-  "corrida",
-  "avaliacao_fisica",
-];
