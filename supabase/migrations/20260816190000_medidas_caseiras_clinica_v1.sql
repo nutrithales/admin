d370e2b7d9a0e3d28fe8372932b127dc6a917829
nutrithales <nutri.thalesrosa@gmail.com>
2026-08-16 18:48:14 -0300

    Adiciona medidas caseiras à Biblioteca Clínica v1

---
+++ supabase/migrations/20260816190000_medidas_caseiras_clinica_v1.sql
@@ -0,0 +1,27 @@
+-- Completa medidas caseiras da Biblioteca Clínica v1.
+-- Mantém gramas como referência principal; medidas caseiras são aproximações práticas.
+
+update public.alimentos set medidas_caseiras='[{"unidade":"Unidade média","peso_g":80}]'::jsonb where biblioteca='clinica_v1' and origem_referencia='BRC0011C';
+update public.alimentos set medidas_caseiras='[{"unidade":"1/2 mamão papaia pequeno","peso_g":150},{"unidade":"Xícara picado","peso_g":140}]'::jsonb where biblioteca='clinica_v1' and origem_referencia='BRC0044C';
+update public.alimentos set medidas_caseiras='[{"unidade":"Colher de sopa cheia","peso_g":25},{"unidade":"Xícara","peso_g":200}]'::jsonb where biblioteca='clinica_v1' and origem_referencia='BRC0158G';
+update public.alimentos set medidas_caseiras='[{"unidade":"Fatia média","peso_g":30},{"unidade":"Colher de sopa cheia","peso_g":25}]'::jsonb where biblioteca='clinica_v1' and origem_referencia='BRC0075N';
+update public.alimentos set medidas_caseiras='[{"unidade":"Fatia média","peso_g":30}]'::jsonb where biblioteca='clinica_v1' and origem_referencia='BRC0036N';
+update public.alimentos set medidas_caseiras='[{"unidade":"Bife médio","peso_g":100}]'::jsonb where biblioteca='clinica_v1' and origem_referencia='BRC0637F';
+update public.alimentos set medidas_caseiras='[{"unidade":"Pegador cheio","peso_g":100},{"unidade":"Xícara cozida","peso_g":140}]'::jsonb where biblioteca='clinica_v1' and origem_referencia='BRC0116A';
+update public.alimentos set medidas_caseiras='[{"unidade":"Unidade pequena","peso_g":80},{"unidade":"Colher de servir cheia","peso_g":100}]'::jsonb where biblioteca='clinica_v1' and origem_referencia='BRC0041B';
+update public.alimentos set medidas_caseiras='[{"unidade":"Unidade média","peso_g":100},{"unidade":"Colher de sopa cheia","peso_g":42}]'::jsonb where biblioteca='clinica_v1' and origem_referencia='BRC0042B';
+update public.alimentos set medidas_caseiras='[{"unidade":"Pedaço médio","peso_g":100},{"unidade":"Colher de sopa cheia","peso_g":37}]'::jsonb where biblioteca='clinica_v1' and origem_referencia='BRC0053B';
+update public.alimentos set medidas_caseiras='[{"unidade":"Colher de sopa cheia","peso_g":25}]'::jsonb where biblioteca='clinica_v1' and origem_referencia='BRC0028T';
+update public.alimentos set medidas_caseiras='[{"unidade":"Colher de sopa cheia","peso_g":25}]'::jsonb where biblioteca='clinica_v1' and origem_referencia='BRC0024T';
+update public.alimentos set medidas_caseiras='[{"unidade":"Colher de sopa cheia","peso_g":20},{"unidade":"Espiga pequena (grãos)","peso_g":80}]'::jsonb where biblioteca='clinica_v1' and origem_referencia='BRC0176A';
+update public.alimentos set medidas_caseiras='[{"unidade":"Unidade pequena","peso_g":60},{"unidade":"Colher de sopa cheia","peso_g":25}]'::jsonb where biblioteca='clinica_v1' and origem_referencia='BRC0047B';
+update public.alimentos set medidas_caseiras='[{"unidade":"Colher de sopa cheia","peso_g":25},{"unidade":"Unidade pequena","peso_g":80}]'::jsonb where biblioteca='clinica_v1' and origem_referencia='BRC0044B';
+update public.alimentos set medidas_caseiras='[{"unidade":"Colher de sopa cheia","peso_g":35},{"unidade":"Pedaço médio","peso_g":100}]'::jsonb where biblioteca='clinica_v1' and origem_referencia='BRC0059B';
+update public.alimentos set medidas_caseiras='[{"unidade":"Folha média","peso_g":10},{"unidade":"Xícara picada","peso_g":35}]'::jsonb where biblioteca='clinica_v1' and origem_referencia='BRC0063B';
+update public.alimentos set medidas_caseiras='[{"unidade":"Unidade média","peso_g":100},{"unidade":"Fatia média","peso_g":20}]'::jsonb where biblioteca='clinica_v1' and origem_referencia='BRC0035B';
+update public.alimentos set medidas_caseiras='[{"unidade":"1/2 unidade média","peso_g":100},{"unidade":"Xícara fatiado","peso_g":100}]'::jsonb where biblioteca='clinica_v1' and origem_referencia='BRC0030B';
+update public.alimentos set medidas_caseiras='[{"unidade":"Xícara cozido","peso_g":90},{"unidade":"Colher de servir cheia","peso_g":45}]'::jsonb where biblioteca='clinica_v1' and origem_referencia='BRC0045B';
+update public.alimentos set medidas_caseiras='[{"unidade":"Fatia média","peso_g":50},{"unidade":"Xícara picada","peso_g":100}]'::jsonb where biblioteca='clinica_v1' and origem_referencia='BRC0156B';
+update public.alimentos set medidas_caseiras='[{"unidade":"Concha rasa","peso_g":80},{"unidade":"Colher de sopa cheia","peso_g":25}]'::jsonb where biblioteca='clinica_v1' and origem_referencia='BRC0275T';
+update public.alimentos set medidas_caseiras='[{"unidade":"Colher de sopa rasa","peso_g":15}]'::jsonb where biblioteca='clinica_v1' and origem_referencia='BRC0002U';
+update public.alimentos set medidas_caseiras='[{"unidade":"Colher de sopa rasa","peso_g":15}]'::jsonb where biblioteca='clinica_v1' and origem_referencia='BRC0290T';
